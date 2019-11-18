66b84aafc00abda2cfc37969e5c5b0ee74929ce5
woshihaoren123 <395668455@qq.com>
2019-11-18 18:49:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <link href="reset.css" rel="stylesheet">
     <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,600" type="text/css" rel="stylesheet">
     <link href="main.css" rel="stylesheet">
+	  
+    <link rel="stylesheet" href="/css/style.css" type="text/css" />
 	
     <meta name="salesforce-community" content="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>">
     <meta name="salesforce-client-id" content="<?php echo getenv('SALESFORCE_CLIENT_ID');?>">
