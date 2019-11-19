0ddb13b84db0b2492c8d07287d17a5cbe2f8ed6b
woshihaoren123 <395668455@qq.com>
2019-11-19 10:20:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,9 @@
     <meta name="salesforce-self-register-enabled" content="<?php echo getenv('SALESFORCE_SELF_REGISTER_ENABLED');?>">
     <meta name="salesforce-login-handler" content="onLogin">
     <meta name="salesforce-logout-handler" content="onLogout">
-	<link href="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=css" rel="stylesheet" type="text/css" />
+    <!--
+    <link href="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=css" rel="stylesheet" type="text/css" />
+    -->
     <script src="/js/sfdc_authmagic.js?type=javascript_widget" async defer></script>
   </head>
   
