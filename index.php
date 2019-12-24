5cd7fb8a652057e53440ac00f2025da4af97e894
woshihaoren123 <395668455@qq.com>
2019-12-24 18:09:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -32,6 +32,7 @@
     <!--
     <script src="/js/sfdc_authmagic.js?type=javascript_widget" async defer></script> -->
 	<script>
+		document.cookie = 'same-site-cookie=foo; SameSite=Lax';
 		document.cookie = 'cross-site-cookie=bar; SameSite=None; Secure';
 	</script>
   </head>
