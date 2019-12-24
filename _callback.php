00ea1c16666c6727ee9cc81e2aec4b60c734803b
woshihaoren123 <395668455@qq.com>
2019-12-24 15:04:39 +0800

    Update _callback.php

--- _callback.php
+++ _callback.php
@@ -6,7 +6,7 @@
 	<meta name="salesforce-save-access-token" content="true">
 	<meta name="salesforce-allowed-domains" content="<?php echo getenv('SALESFORCE_ALLOWED_DOMAINS');?>">
 	<script src="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
-	<script>document.cookie = 'same-site-cookie=foo; SameSite=Lax';
+	<script>
 		document.cookie = 'cross-site-cookie=bar; SameSite=None; Secure';
 	</script>
 </head> 
